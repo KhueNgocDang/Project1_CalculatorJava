bd7fe4fec25cf5e998fce60052cc05706de79926
Khue Dang Ngoc <khue.dn1910442@sis.hust.edu.vn>
2020-04-27 07:52:33 +0700

    Redesigned Sci Cal

--- Project1_Calculator/src/Calculators/MainWindow.java
+++ Project1_Calculator/src/Calculators/MainWindow.java
@@ -645,7 +645,7 @@ public class MainWindow extends JFrame {
 		setBackground(Color.WHITE);
 		setTitle("Calculator - Project 1");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 604, 465);
+		setBounds(100, 100, 612, 462);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -675,6 +675,288 @@ public class MainWindow extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JButton btnZeroButton = new JButton("0");
+		btnZeroButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnZeroButton);
+		btnZeroButton.setBounds(229, 363, 74, 32);
+		contentPane.add(btnZeroButton);
+		
+		JButton btnButtonOne = new JButton("1");
+		btnButtonOne.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonOne);
+		btnButtonOne.setBounds(156, 332, 74, 32);
+		contentPane.add(btnButtonOne);
+		
+		JButton btnButtonTwo = new JButton("2");
+		btnButtonTwo.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonTwo);
+		btnButtonTwo.setBounds(229, 332, 74, 32);
+		contentPane.add(btnButtonTwo);
+		
+		JButton btnButtonThree = new JButton("3");
+		btnButtonThree.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonThree);
+		btnButtonThree.setBounds(302, 332, 74, 32);
+		contentPane.add(btnButtonThree);
+
+		JButton btnButtonFour = new JButton("4");
+		btnButtonFour.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonFour);
+		btnButtonFour.setBounds(156, 301, 74, 32);
+		contentPane.add(btnButtonFour);
+		
+		JButton btnButtonFive = new JButton("5");
+		btnButtonFive.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonFive);
+		btnButtonFive.setBounds(229, 301, 74, 32);
+		contentPane.add(btnButtonFive);
+		
+		JButton btnButtonSix = new JButton("6");
+		btnButtonSix.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonSix);
+		btnButtonSix.setBounds(302, 301, 74, 32);
+		contentPane.add(btnButtonSix);
+		
+		JButton btnButtonSeven = new JButton("7");
+		btnButtonSeven.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonSeven);
+		btnButtonSeven.setBounds(156, 270, 74, 32);
+		contentPane.add(btnButtonSeven);
+		
+		JButton btnButtonEight = new JButton("8");
+		btnButtonEight.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonEight);
+		btnButtonEight.setBounds(229, 270, 74, 32);
+		contentPane.add(btnButtonEight);
+		
+		JButton btnButtonNine = new JButton("9");
+		btnButtonNine.setFont(new Font("Arial", Font.PLAIN, 11));
+		NumberButtonPressed(btnButtonNine);
+		btnButtonNine.setBounds(302, 270, 74, 32);
+		contentPane.add(btnButtonNine);
+		
+		JButton btnPointButton = new JButton(".");
+		btnPointButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnPointButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(!CalDisplayResult.getText().contains(".")) 
+				{
+					CalDisplayResult.setText(CalDisplayResult.getText() + btnPointButton.getText());
+				}
+			}
+		});
+		btnPointButton.setBounds(302, 363, 74, 32);
+		contentPane.add(btnPointButton);
+		
+		JButton btnNegateButton = new JButton("+/-");
+		btnNegateButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnNegateButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(CalDisplayResult.getText()!=null) 
+				{
+					double ops = Double.parseDouble(String.valueOf(CalDisplayResult.getText()));
+					ops = ops *(-1);
+					CalDisplayResult.setText(String.valueOf(ops));
+				}
+			}
+		});
+		btnNegateButton.setBounds(156, 363, 74, 32);
+		contentPane.add(btnNegateButton);
+		
+		JButton btnPiButton = new JButton("π");
+		btnPiButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnPiButton.setBounds(83, 177, 74, 32);
+		contentPane.add(btnPiButton);
+		
+		JButton btnEulersNumberButton = new JButton("e");
+		btnEulersNumberButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnEulersNumberButton.setBounds(156, 177, 74, 32);
+		contentPane.add(btnEulersNumberButton);
+		
+		JButton btnClear_ClearEntry = new JButton("9");
+		btnClear_ClearEntry.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnClear_ClearEntry.setBounds(302, 177, 74, 32);
+		contentPane.add(btnClear_ClearEntry);
+		
+		JButton btnExponentialButton = new JButton("exp");
+		btnExponentialButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnExponentialButton.setBounds(302, 208, 74, 32);
+		contentPane.add(btnExponentialButton);
+		
+		JButton btnAbsoluteButton = new JButton("|x|");
+		btnAbsoluteButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnAbsoluteButton.setBounds(229, 208, 74, 32);
+		contentPane.add(btnAbsoluteButton);
+		
+		JButton btnOpeningParenthesis = new JButton("(");
+		btnOpeningParenthesis.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnOpeningParenthesis.setBounds(156, 239, 74, 32);
+		contentPane.add(btnOpeningParenthesis);
+		
+		JButton btnClosingParenthesis = new JButton(")");
+		btnClosingParenthesis.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnClosingParenthesis.setBounds(229, 239, 74, 32);
+		contentPane.add(btnClosingParenthesis);
+		
+		JButton btnFactorial = new JButton("n!");
+		btnFactorial.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnFactorial.setBounds(302, 239, 74, 32);
+		contentPane.add(btnFactorial);
+		
+		JButton btnReciprocalButton = new JButton("1⁄x");
+		btnReciprocalButton.setBounds(156, 208, 74, 32);
+		contentPane.add(btnReciprocalButton);
+		
+		JButton btnPercentageButton = new JButton("%");
+		btnPercentageButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnPercentageButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				String tag = null;
+				switch(mark3) 
+				{
+					case 0: mark3 = 1;tag="0";break;
+					case 1: mark3 = 0;tag="1";break;
+					
+				}
+				
+				CalDisplayResult.setText(tag);
+			}
+		});
+		btnPercentageButton.setBounds(10, 177, 74, 32);
+		contentPane.add(btnPercentageButton);
+		
+		
+		JButton btnLevel1 = new JButton();
+		btnLevel1.setFont(new Font("Arial", Font.PLAIN, 11));
+		if(mark3 == 0) 
+		{
+			
+			btnLevel1.setText("1");;
+		}
+		else btnLevel1.setText("0");
+		btnLevel1.setBounds(10, 208, 74, 32);;
+		contentPane.add(btnLevel1);
+		
+		JButton btnLevel2 = new JButton("cube");
+		btnLevel2.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel2.setBounds(10, 239, 74, 32);
+		contentPane.add(btnLevel2);
+		
+		JButton btnLevel3 = new JButton("x^y");
+		btnLevel3.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel3.setBounds(10, 270, 74, 32);
+		contentPane.add(btnLevel3);
+		
+		JButton btnLevel4 = new JButton("10^x");
+		btnLevel4.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel4.setBounds(10, 301, 74, 32);
+		contentPane.add(btnLevel4);
+		
+		JButton btnLevel5 = new JButton("log");
+		btnLevel5.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel5.setBounds(10, 332, 74, 32);
+		contentPane.add(btnLevel5);
+		
+		JButton btnLevel6 = new JButton("ln");
+		btnLevel6.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel6.setBounds(10, 363, 74, 32);
+		contentPane.add(btnLevel6);
+		
+		JButton btnDeleteButton = new JButton("←");
+		btnDeleteButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnDeleteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String bsp = null;
+				if(CalDisplayInput.getText().length()>0) {
+					StringBuilder strB = new StringBuilder(CalDisplayInput.getText());
+					strB.deleteCharAt(CalDisplayInput.getText().length() - 1);
+					bsp = strB.toString();
+					CalDisplayInput.setText(bsp);
+				}
+			}
+		});
+		btnDeleteButton.setBounds(376, 177, 74, 32);
+		contentPane.add(btnDeleteButton);
+		
+		JButton btnModularButton = new JButton("mod");
+		btnModularButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnModularButton.setBounds(376, 208, 74, 32);
+		contentPane.add(btnModularButton);
+		
+		JButton btnDivideButton = new JButton("÷");
+		btnDivideButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		OperatorButtonPressed(btnDivideButton);
+		btnDivideButton.setBounds(376, 239, 74, 32);
+		contentPane.add(btnDivideButton);
+		
+		JButton btnMultiplyButton = new JButton("×");
+		btnMultiplyButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		OperatorButtonPressed(btnMultiplyButton);
+		btnMultiplyButton.setBounds(376, 270, 74, 32);
+		contentPane.add(btnMultiplyButton);
+		
+		JButton btnMinusButton = new JButton("-");
+		btnMinusButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		OperatorButtonPressed(btnMinusButton);
+		btnMinusButton.setBounds(376, 301, 74, 32);
+		contentPane.add(btnMinusButton);
+		
+		JButton btnPlusButton = new JButton("+");
+		btnPlusButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		OperatorButtonPressed(btnPlusButton);
+		btnPlusButton.setBounds(376, 332, 74, 32);
+		contentPane.add(btnPlusButton);
+		
+		JButton btnEqualButton = new JButton("=");
+		btnEqualButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		EqualButtonPressed(btnEqualButton);
+		btnEqualButton.setBounds(376, 363, 74, 32);
+		contentPane.add(btnEqualButton);
+		
+		JMenu mnNewMenu = new JMenu("New menu");
+		mnNewMenu.setBounds(10, 137, 113, 27);
+		contentPane.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("New menu");
+		mnNewMenu_1.setBounds(134, 137, 113, 27);
+		contentPane.add(mnNewMenu_1);
+		
+		JButton btn2ndmode_1 = new JButton("2nd");
+		btn2ndmode_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btn2ndmode_1.setBounds(229, 177, 74, 32);
+		contentPane.add(btn2ndmode_1);
+		
+		JButton btnLevel1_1 = new JButton();
+		btnLevel1_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel1_1.setBounds(83, 208, 74, 32);
+		contentPane.add(btnLevel1_1);
+		
+		JButton btnLevel2_1 = new JButton("cube");
+		btnLevel2_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel2_1.setBounds(83, 239, 74, 32);
+		contentPane.add(btnLevel2_1);
+		
+		JButton btnLevel4_1 = new JButton("10^x");
+		btnLevel4_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel4_1.setBounds(83, 301, 74, 32);
+		contentPane.add(btnLevel4_1);
+		
+		JButton btnLevel3_1 = new JButton("x^y");
+		btnLevel3_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel3_1.setBounds(83, 270, 74, 32);
+		contentPane.add(btnLevel3_1);
+		
+		JButton btnLevel5_1 = new JButton("log");
+		btnLevel5_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel5_1.setBounds(83, 332, 74, 32);
+		contentPane.add(btnLevel5_1);
+		
+		JButton btnLevel6_1 = new JButton("ln");
+		btnLevel6_1.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnLevel6_1.setBounds(83, 363, 74, 32);
+		contentPane.add(btnLevel6_1);
 		
 	}
 }
